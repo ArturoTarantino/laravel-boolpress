2889bd13484dacdf71e9fecad4df203239b75e25
ArturoTarantino <arturotarantino92@gmail.com>
2022-02-21 17:27:23 +0100

    scaffolding front-office

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
 Route::middleware('auth')
@@ -27,3 +24,7 @@ Route::middleware('auth')
         Route::get('/', 'HomeController@index')
         ->name('home');
     });
+
+Route::get('{any?}', function(){
+    return view('guest.home');
+})->where('any', '.*');
