c2d057e7a45361c692b0d4f4936967941e7c1e01
ArturoTarantino <arturotarantino92@gmail.com>
2022-02-24 13:24:05 +0100

    created user info

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -3,4 +3,9 @@
 @section('content')
     
     <h1>Benvenuto {{ $user->name }}</h1>
+    {{-- {{ dd($user->userInfo()) }} --}}
+    @if ($user->userInfo)
+        <div>Telefono: {{ $user->userInfo->phone }}</div>
+        <div>Indirizzo: {{ $user->userInfo->address }}</div>
+    @endif
 @endsection
