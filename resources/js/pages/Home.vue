72cd4e3294337d9618c8bf92563f33fc5762541d
ArturoTarantino <arturotarantino92@gmail.com>
2022-03-03 15:39:51 +0100

    installed vue router and created js/pages/Home

---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,13 @@
+<template>
+    <section>
+        <div class="container">
+            <h1>home page</h1>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'Home'
+}
+</script>
--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,16 +1,14 @@
 <template>
     <div>
-        <Posts />
+        <main>
+            <router-view></router-view>
+        </main>
     </div>
 </template>
 
 <script>
-import Posts from '../components/Posts.vue';
 
 export default {
     name:'App',
-    components: {
-        Posts
-    }
 }
 </script>
